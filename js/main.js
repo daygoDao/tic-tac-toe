9d6fa5db6073f450c180a7632f60adffdb412bdd
dao <gary.vo32@gmail.com>
2022-03-23 08:36:59 -0700

    able to alternate X and O symbols when marking up gameboard

--- js/main.js
+++ js/main.js
@@ -19,19 +19,57 @@ function markSpot(e) {
   if (e.target.textContent !== '') {
     console.log('already taken');
   } else {
-    e.target.textContent = 'X'
-    console.log('marked!')
+    // x goes first then o
+    if (playerX.moveCounter == playerO.moveCounter) {
+      e.target.textContent = 'X'
+      playerX.moveCounter++;
+      console.log(playerX.moveCounter)
+    } else if (playerX.moveCounter > playerO.moveCounter) {
+      e.target.textContent = 'O'
+      playerO.moveCounter++;
+      console.log(playerO.moveCounter)
+    }
   }
 
   //check if winner is found
 
 }
 
+// player object
+const player = (faction) => {
+  let moveCounter = 0;
+  this.faction = faction;
+  return {
+    moveCounter,
+    faction
+  };
+}
+
+//create player 1 and 2
+const playerO = player('O');
+const playerX = player('X');
+
+
+
+
+
+
 // start game
 document.querySelector('.start-it').addEventListener('click', start);
 
+
 function start(e) {
   //console.log(e);
+  let count = 0;
+
+  if (count % 2 == 0) {
+    //markSpot(playerO);
+    count++;
+  } else {
+    //markSpot(playerX);
+    count++;
+  }
+
   //reset gameboard to blank
   let squares = document.querySelectorAll('.territory')
   for (let square of squares) {
@@ -42,6 +80,5 @@ function start(e) {
     square.addEventListener('click', markSpot);
   }
 
-  //create player 1 and 2
 
 }
